7a609685178b2b4d2b5e0301e4d556d036eb5b6b
Arunkumar032024 <arunkumar032024@gmail.com>
2024-07-15 11:06:18 +0530

    Add draggable interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,19 @@ $(document).ready(function(){
     xMark.click(function(){
         nav.slideUp();
     })
+
+
+    // interaction
+
+    // all section are hide 
+    $('section').hide();
+
+    const draggable = $("a[href='#draggable']");
+    draggable.click(function(){
+        if(window.innerWidth < 768){
+            nav.slideUp();
+        }
+        $('section').slideUp();
+        $('section#draggable').slideDown();
+    })
 })
